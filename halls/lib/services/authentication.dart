0b41ed258676c3d2aafc4636758ff1f573494928
BenjaNak <benjaminchris528@gmail.com>
2024-07-22 10:58:20 +0300

    added animation and profile

--- halls/lib/services/authentication.dart
+++ halls/lib/services/authentication.dart
@@ -1,50 +1,45 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthMethod {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // SignUp User
-
+  // Sign up user
   Future<String> signupUser({
     required String email,
     required String password,
     required String name,
   }) async {
-    String res = "Some error Occurred";
+    String res = "Some error occurred";
     try {
       if (email.isNotEmpty || password.isNotEmpty || name.isNotEmpty) {
-        // register user in auth with email and password
+        // Register user with email and password
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
           email: email,
           password: password,
         );
-        // add user to your  firestore database
-        print(cred.user!.uid);
-        await _firestore.collection("users").doc(cred.user!.uid).set({
-          'name': name,
-          'uid': cred.user!.uid,
-          'email': email,
-        });
+
+        // Update user display name
+        await cred.user!.updateProfile(displayName: name);
+        await _auth.currentUser!.reload(); // Reload the user
+        User? updatedUser = _auth.currentUser; // Get the updated user
 
         res = "success";
       }
     } catch (err) {
-      return err.toString();
+      res = err.toString();
     }
     return res;
   }
 
-  // logIn user
+  // Log in user
   Future<String> loginUser({
     required String email,
     required String password,
   }) async {
-    String res = "Some error Occurred";
+    String res = "Some error occurred";
     try {
       if (email.isNotEmpty || password.isNotEmpty) {
-        // logging in user with email and password
+        // Logging in user with email and password
         await _auth.signInWithEmailAndPassword(
           email: email,
           password: password,
@@ -54,13 +49,18 @@ class AuthMethod {
         res = "Please enter all the fields";
       }
     } catch (err) {
-      return err.toString();
+      res = err.toString();
     }
     return res;
   }
 
-  // for sighout
- Future <void> signOut() async {
+  // Sign out user
+  Future<void> signOut() async {
     await _auth.signOut();
+  }
+
+  // Get current user details
+  User? getCurrentUser() {
+    return _auth.currentUser;
   }
 }
